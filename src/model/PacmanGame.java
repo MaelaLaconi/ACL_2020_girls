731537d860e7ef65cf3160b038ec397364f784e9
Nada <madadnada@gmail.com>
2020-10-10 21:09:10 +0200

    Update sprint1

--- src/model/PacmanGame.java
+++ src/model/PacmanGame.java
@@ -61,17 +61,21 @@ private int x;
 	 */
 	@Override
 	public void evolve(Cmd commande) {
-		System.out.println("Execute "+commande);
-		if (commande==Cmd.LEFT){
+		//System.out.println("Execute "+commande);
+		if (commande==Cmd.LEFT && x>=0){
 			x--;
+			System.out.println("Pacman("+x+","+y+")");
 		}
-		if (commande==Cmd.UP){
+		if (commande==Cmd.UP && y>=0){
 			y--;
+			System.out.println("Pacman("+x+","+y+")");
 		}
-		if (commande==Cmd.RIGHT){
+		if (commande==Cmd.RIGHT ){
 			x++;
+			System.out.println("Pacman("+x+","+y+")");
 		}
-		if (commande==Cmd.DOWN){
+		if (commande==Cmd.DOWN ){
+			System.out.println("Pacman("+x+","+y+")");
 			y++;
 		}
 	}
--- src/start/Main.java
+++ src/start/Main.java
@@ -1,9 +1,12 @@
 package start;
 
+import engine.Cmd;
 import model.PacmanPainter;
 import engine.GameEngineGraphical;
 import model.PacmanController;
 import model.PacmanGame;
+
+import java.util.Scanner;
 
 /**
  * lancement du moteur avec le jeu
@@ -12,13 +15,19 @@ public class Main {
 
 	public static void main(String[] args) throws InterruptedException {
 
+		Scanner sc=new Scanner(System.in);
+
+		System.out.println("Ecrire Command(L/R/U/D/S)");
+		String cmd=sc.next();
+
+
 		// creation du jeu particulier et de son afficheur
 		PacmanGame game = new PacmanGame("helpFilePacman.txt");
 		PacmanPainter painter = new PacmanPainter(game);
 		PacmanController controller = new PacmanController();
 
 		// classe qui lance le moteur de jeu generique
-		GameEngineGraphical engine = new GameEngineGraphical(game, painter, controller);
+		GameEngineGraphical engine = new GameEngineGraphical(game, painter, controller,cmd);
 		engine.run();
 	}
 
