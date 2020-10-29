52c3d5488c55aaf4899b54b92bd227c7cab6b647
Nada <madadnada@gmail.com>
2020-10-29 16:00:45 +0100

    update sprint

---
+++ src/engine/GamePainter.java
@@ -0,0 +1,27 @@
+package engine;
+
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
+/**
+ * @author Horatiu Cirstea, Vincent Thomas
+ * 
+ * represente la maniere de dessiner sur un JPanel
+ * 
+ */
+public interface GamePainter {
+
+	/**
+	 * methode dessiner a completer. Elle construit une image correspondant au
+	 * jeu. Game est un attribut de l'afficheur
+	 * 
+	 * @param image
+	 *            image sur laquelle dessiner
+	 */
+	public abstract void draw(BufferedImage image) throws IOException;
+
+	public abstract int getWidth();
+
+	public abstract int getHeight();
+	
+}
